7cdafeb42a1c458e23e7866c0557731657a94c43
nobody <nobody@3edca8a7-7d0a-0410-8186-e30a1c544c73>
2005-10-16 19:59:27 +0000

    This commit was manufactured by cvs2svn to create tag 'version_1_15'.

---
+++ tags/version_1_15/dejavu-fonts/makedist.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# $Id$
+
+version=$1
+echo Making distribution of DejaVu fonts $version
+mkdir packaged
+mkdir packaged/dejavu-sfd-$version
+cp *.sfd *.pe *.me *.sh *.pl README LICENSE AUTHORS NEWS BUGS *.txt packaged/dejavu-sfd-$version
+(cd packaged; tar czvf dejavu-sfd-$version.tar.gz dejavu-sfd-$version)
+mkdir packaged/dejavu-ttf-$version
+cp generated/*.ttf README LICENSE AUTHORS NEWS BUGS *.txt packaged/dejavu-ttf-$version
+(cd packaged; tar czvf dejavu-ttf-$version.tar.gz dejavu-ttf-$version)
+(cd packaged; zip -rv dejavu-ttf-$version.zip dejavu-ttf-$version)
